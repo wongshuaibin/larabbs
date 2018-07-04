f2a6103967a94115f188cebe7c85c7f3799e8b90
wongshuaibin <wongshuaibin@gmail.com>
2018-07-04 13:06:24 +0000

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -23,5 +23,10 @@ class ReplyObserver
     {
         $reply->content = clean($reply->content, 'user_topic_body');
     }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
+    }
 }
 
