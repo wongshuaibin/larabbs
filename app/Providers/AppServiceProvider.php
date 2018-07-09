fd839e6fdbd6f037b2a22c08f6ac6e6ac483acdb
wongshuaibin <wongshuaibin@gmail.com>
2018-07-09 10:27:39 +0000

    Add manage contents permission and visit horizon dashboard permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,11 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+
     public function boot()
 	{
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
@@ -22,13 +18,11 @@ class AppServiceProvider extends ServiceProvider
         \Carbon\Carbon::setLocale('zh');
     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+
     public function register()
     {
-        //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
     }
 }
