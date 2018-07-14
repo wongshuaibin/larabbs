cce638971c95a6398225c832b5670694f74cda85
wongshuaibin <wongshuaibin@gmail.com>
2018-07-14 14:54:07 +0000

    Keep track of user last visited time

---
+++ database/migrations/2018_07_14_220914_add_last_actived_at_to_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLastActivedAtToUsersTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('last_actived_at')->nullable();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColum('last_actived_at');
+        });
+    }
+}
